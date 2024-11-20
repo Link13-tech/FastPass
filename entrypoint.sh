32e9f01c730618e8b2f852c6c9d1e4970776a661
Link13-tech <151656048+Link13-tech@users.noreply.github.com>
2024-11-20 21:15:01 +0300

    hope 1 vers

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,20 +1,25 @@
 #!/bin/sh
 
 echo "Проверяем состояние миграций..."
-
-# Получаем текущую версию миграций в базе данных
+MIGRATION_FILES=$(find alembic/versions -type f | wc -l)
+# Проверяем текущую версию миграций в базе
 CURRENT_VERSION=$(poetry run alembic current)
 
-# Если версия не найдена или это первый запуск, создаем миграцию
 if [ -z "$CURRENT_VERSION" ]; then
-    echo "Миграции не найдены или это первый запуск, создаем и применяем миграции..."
-    # Создаем миграцию и применяем её
+    # База пустая, создаём первую миграцию и применяем её
+    echo "База пустая. Создаём первую миграцию..."
     poetry run alembic revision --autogenerate -m "Initial migration"
+    poetry run alembic upgrade head
+elif [ "$MIGRATION_FILES" -eq 0 ] && [ -n "$CURRENT_VERSION" ]; then
+    # Папка пуста, но база имеет миграцию, синхронизируем с помощью stamp
+    echo "Папка версий пуста, синхронизируем состояние с базой..."
+    poetry run alembic stamp head   # Обновляем таблицу alembic_version, чтобы указать, что миграция уже применена
+    echo "Создаём новую миграцию с учётом изменений в моделях(есл они были)..."
+    poetry run alembic revision --autogenerate -m "Auto migration update"
     poetry run alembic upgrade head
 else
     echo "Миграции уже применены. Текущая версия: $CURRENT_VERSION"
 fi
 
-# Запускаем приложение
 echo "Запускаем приложение..."
 poetry run uvicorn main:app --host 0.0.0.0 --port 8080
