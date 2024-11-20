ccc8327c5adeef695e80a8705e72b39a443304a4
Link13-tech <151656048+Link13-tech@users.noreply.github.com>
2024-11-20 03:52:39 +0300

    fix entrypoint.sh last ver

--- entrypoint.sh
+++ entrypoint.sh
@@ -9,6 +9,5 @@ else
     poetry run alembic upgrade head
 fi
 
-# Запускаем приложение
-echo "Запуск приложения..."
-uvicorn main:app --host 0.0.0.0 --port 8080
+echo "Запускаем приложение..."
+poetry run uvicorn main:app --host 0.0.0.0 --port 8080
